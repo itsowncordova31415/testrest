e81e4c0de79b13c23b0a55de235205ad3da2d16c
itsowncordova31415 <46975907+itsowncordova31415@users.noreply.github.com>
2019-01-24 19:06:48 +0700

    pull

---
+++ RestAPI/api/.idea/index.php
@@ -0,0 +1,9 @@
+<form  action="http://restapi/api/User/signup.php" method="post">
+    Email: <input type="text" name="email"><br>
+    Телефон: <input type="text" name="phone"><br>
+    Текст : <input type="text" name="text"><br>
+    <input type="submit">
+</form>
+
+</body>
+</html>
